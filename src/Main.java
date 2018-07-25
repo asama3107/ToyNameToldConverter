2e8bfc28a937a43e7853c69a25a30802f1365298
Asama Usuri <asamausuri@MacBook-Pro-Asama.local>
2018-07-25 14:36:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ToldConverter convert = new ToldConverter();
+        convert.toyName (0);
+        convert.toyName (1);
+        convert.toyName (2);
+        convert.toyName (3);
+        convert.toyName (4);
+
+    }
+
+}
---
+++ src/ToldConverter.java
@@ -0,0 +1,27 @@
+public class ToldConverter {
+
+    public void toyName (int toy){
+
+
+
+        switch(toy)
+        {
+            case  0:
+                System.out.println("Car");
+                break;
+            case 1:
+                System.out.println("Lego");
+                break;
+            case 2 :
+                System.out.println("Doll");
+                break;
+            case 3 :
+                System.out.println("Puzzle");
+                break;
+            default :
+                System.out.println("Illegal");
+        }
+
+    }
+
+}
